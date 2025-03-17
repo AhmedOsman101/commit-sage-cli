e149b6bfd0890be5aaef3775588ee4a9a4663040
AhmedOsman101 <trollingosman101@outlook.com>
2025-03-17 03:32:14 +0200

    feat(ai): fetch diff and changed files
    - Init git service
    - Get staged changes
    - Fetch diff
    - Fetch changed files

--- services/aiService.ts
+++ services/aiService.ts
@@ -1,5 +1,20 @@
+import { logError } from "../utils/Logger.ts";
+import ConfigService from "./configService.ts";
+import GitService from "./gitService.ts";
+
 const AiService = {
-  async generateAndApplyMessage() {},
+  async generateAndApplyMessage() {
+    await GitService.initialize();
+    const onlyStagedSetting = ConfigService.get("onlyStaged") as boolean;
+    const hasStagedChanges = await GitService.hasChanges("staged");
+
+    const useStagedChanges = onlyStagedSetting || hasStagedChanges;
+
+    const diff = await GitService.getDiff(useStagedChanges);
+    if (!diff) logError("No changes to commit");
+
+    const changedFiles = await GitService.getChangedFiles(useStagedChanges);
+  },
 };
 
 export default AiService;
