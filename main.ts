e6f45b2d8de15ce2d788e364d932af95d17c75ae
AhmedOsman101 <trollingosman101@outlook.com>
2025-04-11 01:51:03 +0200

    feat: main functionality
    - Added main function
    - Implemented error handling
    - Called AI service

--- main.ts
+++ main.ts
@@ -1,9 +1,29 @@
-import { logInfo } from "./utils/Logger.ts";
+import AiService from "./services/aiService.ts";
+import GitService from "./services/gitService.ts";
+import { logError } from "./utils/Logger.ts";
 
-export const scriptDir = import.meta.dirname;
+const homedir = Deno.env.get("HOME");
 
-export let repoPath: string;
+export const configPath = `${homedir}/commitSage/config.json`;
+
+export const repoPath = await GitService.initialize();
+
+async function main(): Promise<void> {
+  try {
+    await GitService.initialize();
+  } catch (error) {
+    void logError("Failed during initialization:", error as Error);
+    return;
+  }
+
+  try {
+    const response = await AiService.generateAndApplyMessage();
+    console.log(response.message);
+  } catch (error) {
+    void logError(error);
+  }
+}
 
 if (import.meta.main) {
-  logInfo("Running at", Date.now());
+  await main();
 }
