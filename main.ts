28314dfb977ec456c141c6e53415e27945a43b52
AhmedOsman101 <trollingosman101@outlook.com>
2025-04-12 17:13:46 +0200

    chore: add copyright and license information

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+// Copyright (C) 2025 Ahmad Othman
+// Licensed under the GNU General Public License v3.0. See LICENSE for details.
+
 import AiService from "./services/aiService.ts";
 import GitService from "./services/gitService.ts";
 import { logError } from "./utils/Logger.ts";
