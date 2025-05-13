008a37c4a1848ffa8686d36bb88a5e722e7423d9
AhmedOsman101 <trollingosman101@outlook.com>
2025-05-13 13:56:06 +0300

    feat: Improve Result type and add Text2Err function
    - Added OkState and ErrorState types
    - Added Text2Err function
    - Improved Result type definition

--- lib/result.ts
+++ lib/result.ts
@@ -1,12 +1,17 @@
-// Rust's Result enum simulation
-export type Result<T, E extends Error = Error> =
-  | { ok: T; error: undefined }
-  | { ok: undefined; error: E };
+type OkState<T> = { ok: T; error: undefined };
+type ErrorState<E extends Error = Error> = { ok: undefined; error: E };
 
-export function Ok<T, E extends Error = Error>(ok: T): Result<T, E> {
+// Rust's Result enum simulation
+export type Result<T, E extends Error = Error> = OkState<T> | ErrorState<E>;
+
+export function Ok<T>(ok: T): OkState<T> {
   return { ok, error: undefined };
 }
 
-export function Err<T, E extends Error = Error>(error: E): Result<T, E> {
+export function Err<E extends Error = Error>(error: E): ErrorState<E> {
   return { ok: undefined, error };
+}
+
+export function Text2Err(message: string): ErrorState<Error> {
+  return { ok: undefined, error: new Error(message) };
 }
