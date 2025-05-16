0101a3e382d9137a2932d3e3e2427e624bc79c2a
AhmedOsman101 <trollingosman101@outlook.com>
2025-05-16 20:46:33 +0300

    build: rename compiled binaries for clarity
    - Updated binary names
    - Ensured consistency across platforms
    - update release.yml

--- release.sh
+++ release.sh
@@ -13,12 +13,12 @@ if [[ ! -d "${baseDir}/bin" ]]; then
 fi
 
 # --- Linux --- #
-deno compile -A -o bin/linux-x64/commit-sage --target x86_64-unknown-linux-gnu main.ts
-deno compile -A -o bin/linux-arm64/commit-sage --target aarch64-unknown-linux-gnu main.ts
+deno compile -A -o bin/commit-sage-linux-x64 --target x86_64-unknown-linux-gnu main.ts
+deno compile -A -o bin/commit-sage-linux-arm64 --target aarch64-unknown-linux-gnu main.ts
 
 # --- Windows --- #
-deno compile -A -o bin/windows-x64/commit-sage --target x86_64-pc-windows-msvc main.ts
+deno compile -A -o bin/commit-sage-windows-x64.exe --target x86_64-pc-windows-msvc main.ts
 
 # --- MacOS --- #
-deno compile -A -o bin/macos-x64/commit-sage --target x86_64-apple-darwin main.ts
-deno compile -A -o bin/macos-arm64/commit-sage --target aarch64-apple-darwin main.ts
+deno compile -A -o bin/commit-sage-macos-x64 --target x86_64-apple-darwin main.ts
+deno compile -A -o bin/commit-sage-macos-arm64 --target aarch64-apple-darwin main.ts
