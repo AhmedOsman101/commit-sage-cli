9de69a93e9d4981b5813ce1b11be4cc8f73be70b
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-06-02 21:04:05 +0300

    refactor: migrate to src directory, update deps
    - Moved source files to `src/` directory
    - Updated dependencies in `deno.lock`
    - Updated compile commands in `deno.json`

--- release.sh
+++ release.sh
@@ -13,12 +13,12 @@ if [[ ! -d "${baseDir}/bin" ]]; then
 fi
 
 # --- Linux --- #
-deno compile -A -o bin/commit-sage-linux-x64 --target x86_64-unknown-linux-gnu main.ts
-deno compile -A -o bin/commit-sage-linux-arm64 --target aarch64-unknown-linux-gnu main.ts
+deno compile -A -o bin/commit-sage-linux-x64 --target x86_64-unknown-linux-gnu src/main.ts
+deno compile -A -o bin/commit-sage-linux-arm64 --target aarch64-unknown-linux-gnu src/main.ts
 
 # --- Windows --- #
-deno compile -A -o bin/commit-sage-windows-x64.exe --target x86_64-pc-windows-msvc main.ts
+deno compile -A -o bin/commit-sage-windows-x64.exe --target x86_64-pc-windows-msvc src/main.ts
 
 # --- MacOS --- #
-deno compile -A -o bin/commit-sage-macos-x64 --target x86_64-apple-darwin main.ts
-deno compile -A -o bin/commit-sage-macos-arm64 --target aarch64-apple-darwin main.ts
+deno compile -A -o bin/commit-sage-macos-x64 --target x86_64-apple-darwin src/main.ts
+deno compile -A -o bin/commit-sage-macos-arm64 --target aarch64-apple-darwin src/main.ts
